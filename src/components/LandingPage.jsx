7e6a722bf4583c82c300bf1dd5bff1e3ed535378
vdsn0905 <vdsn0905@gmail.com>
2025-09-10 21:30:19 +0530

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -20,9 +20,9 @@ export default function LandingPage() {
       {!showSplash && (
         <div className="min-h-screen bg-gradient-to-br justify-center from-gray-900 via-gray-800 to-gray-900 flex flex-col">
           <header className="w-full py-8 flex flex-col items-center">
-            <h1 className="text-white">ARCLANG Editor</h1>
+            <h1 className="text-white">LangForage Editor</h1>
             <p className="text-lg md:text-xl text-gray-300 mb-4 text-center max-w-xl">
-              Learn, practice, and master programming in ARCLANG with
+              Learn, practice, and master programming in LangForage with
               interactive challenges and a modern online IDE.
             </p>
           </header>
@@ -31,4 +31,5 @@ export default function LandingPage() {
     </div>
   );
 }
+
 
